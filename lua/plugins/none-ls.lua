602b10205688487007e30ac21a2e4e638df760f9
khun <khunor22@gmail.com>
2024-12-02 20:31:49 +0100

    sync: 2024-12-02 20:31:49

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,7 @@ return {
         null_ls.setup {
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.completion.spell,
+                --null_ls.builtins.completion.spell,
                 --                require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
             },
         }
