4128d476c181af3bd4b024de59a0d0a85c58a577
khun2 <khunor22@gmail.com>
2024-12-14 08:46:54 +0100

    2024. dec. 14., szombat, 08:46:54 CET

--- init.lua
+++ init.lua
@@ -1,26 +1,21 @@
 require 'config.lazy'
---require 'lua.lsp_diagnostics_options_setup'
-
---Set completeopt to have a better completion experience
--- :help completeopt
--- menuone: popup even when there's only one match
--- noinsert: Do not insert text until a selection is made
--- noselect: Do not select, force to select one from the menu
--- shortness: avoid showing extra messages when using completion
--- updatetime: set updatetime for CursorHold
-vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
 vim.opt.shortmess = vim.opt.shortmess + { c = true }
 vim.api.nvim_set_option('updatetime', 300)
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
 
--- Fixed column for diagnostics to appear
--- Show autodiagnostic popup on cursor hover_range
--- Goto previous / next diagnostic warning / error
--- Show inlay_hints more frequently
 vim.cmd [[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]]
 
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.cmd 'set expandtab'
 vim.cmd 'set tabstop=4'
 vim.cmd 'set softtabstop=4'
@@ -29,6 +24,13 @@ vim.cmd 'set number'
 vim.cmd 'set relativenumber'
 vim.cmd 'set cursorline'
 vim.cmd 'colorscheme tokyonight-night'
+vim.cmd 'set scrolloff=6'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.undofile = true
+vim.opt.breakindent = true
+vim.opt.timeoutlen = 300
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal right<CR>')
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,62 +1,59 @@
 return {
-    {
-        --rust lsp setup, may split into different file
-        'simrat39/rust-tools.nvim',
-        config = function()
-            local rt = require 'rust-tools'
+  {
+    --rust lsp setup, may split into different file
+    'simrat39/rust-tools.nvim',
+    config = function()
+      local rt = require 'rust-tools'
 
-            rt.setup {
-                server = {
-                    on_attach = function(_, bufnr)
-                        -- Hover actions
-                        vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
-                        -- Code action groups
-                        vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-                    end,
-                },
-            }
-        end,
-    },
-    {
-        'williamboman/mason.nvim',
-        lazy = false,
-        config = function()
-            require('mason').setup()
-        end,
-    },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        lazy = false,
-        config = function()
-            require('mason-lspconfig').setup {
-                auto_install = true,
-            }
-        end,
-    },
-    {
-        'neovim/nvim-lspconfig',
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      rt.setup {
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      }
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = function()
+      require('mason').setup()
+    end,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    lazy = false,
+    config = function()
+      require('mason-lspconfig').setup {
+        auto_install = true,
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            local lspconfig = require 'lspconfig'
-            lspconfig.lua_ls.setup {
-                capabilities = capabilities,
-            }
+      local lspconfig = require 'lspconfig'
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+      }
 
-            require('lspconfig').clangd.setup {
-                capabilities = capabilities,
-            }
+      require('lspconfig').clangd.setup {
+        capabilities = capabilities,
+      }
 
-            require('lspconfig').rust_analyzer.setup {
-                capabilities = capabilities,
-            }
-            require('lspconfig').bacon_ls.setup {
-                capabilities = capabilities,
-            }
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end,
-    },
+      require('lspconfig').rust_analyzer.setup {
+        capabilities = capabilities,
+      }
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end,
+  },
 }
